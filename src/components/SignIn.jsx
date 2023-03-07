7f50f045a93a16dac26bb94f83b497382838a195
Balaganesh003 <k.balaganesh26@gmail.com>
2023-03-07 22:16:24 +0530

    Made the signIn page mobile responsive

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import GoogleLogo from '../../public/google-icon.svg';
 import TwitterLogo from '../../public/twitter-logo.svg';
 import Image from 'next/image';
+import QuiddleLogo from '../../public/quiddle-logo.svg';
 
 const SignIn = () => {
   return (
-    <div className="bg-[#0c1324] min-h-screen min-w-screen py-[8rem] px-[5rem]">
+    <div className="bg-[#0c1324] h-screen min-h-fit py-[8rem] lg:px-[5rem] ">
       <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className=" h-56 w-56 absolute rounded-full bg-gradient-to-br from-blue-600 to-blue-400 right-[3rem] bottom-[5rem]"></div>
-        <div className="h-56 w-56 absolute rounded-full bg-gradient-to-r from-orange-500 to-yellow-400 left-[3rem] top-[5rem]"></div>
+        <div className="h-40 w-40 sm:h-56 sm:w-56 absolute  animate-pulse rounded-full bg-gradient-to-br from-blue-600 to-blue-400 sm:right-[3rem] -right-[1rem] bottom-[5rem]"></div>
+        <div className="h-40 w-40 sm:h-56 sm:w-56 absolute animate-pulse rounded-full bg-gradient-to-r from-orange-500 to-yellow-400 sm:left-[3rem] -left-[1rem]  top-[5rem]"></div>
       </div>
-      <form className=" w-[24rem] min-w-screen  bg-opacity-25 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xl backdrop-filter backdrop-blur-lg border-2 border-white border-opacity-10 shadow-lg py-8 px-7">
-        <h1 className="text-white text-3xl text-center mb-4">Quiddle</h1>
+      <form className="sm:w-[24rem] w-[90%]  bg-opacity-25 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-xl backdrop-blur-lg border-2 border-white border-opacity-10 shadow-lg py-8 px-7">
+        <div className="text-white flex items-center mx-auto w-[15rem] justify-center mb-4">
+          <Image src={QuiddleLogo} alt="logo" className="" />
+        </div>
         <h3 className="text-base font-medium text-white/80 text-center mb-8">
           Sign In to your account
         </h3>
@@ -37,7 +40,7 @@ const SignIn = () => {
         />
 
         <button className="w-full h-10 bg-white text-black font-medium rounded-md hover:bg-gray-200 transition-colors">
-          Sign Up
+          Sign In
         </button>
         <p className="text-white text-center my-2">Or</p>
         <div className="flex gap-7">
