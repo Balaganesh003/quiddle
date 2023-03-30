e2d9256793291185080555b128b6c76877640d2d
Balaganesh003 <k.balaganesh26@gmail.com>
2023-03-30 15:44:35 +0530

    up[dated the styles for the header for the mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div className="bg-[#0c1324] text-white h-screen min-h-fit py-[8rem] lg:px-[5rem]">
-      <div className="w-full h-full flex gap-3 flex-col items-center ">
+    <div className="bg-[#0c1324] px-4 text-white h-screen min-h-fit py-[1rem] lg:px-[5rem]">
+      <div className="h-full w-full flex flex-col items-center  justify-center">
         <h1 className="text-center uppercase ">Word Game Like Never Before</h1>
-        <div className="w-full h-fit">
-          <svg viewBox="0 0 2100 350">
+        <div className="w-full h-fit hidden sm:block">
+          <svg viewBox="0 0 2100 400 ">
             <text
-              className="text-[13rem] md:text-[22rem] lg:text-15xl"
+              className="text-[25rem]"
               x="50%"
               y="50%"
               dy=".4em"
@@ -21,7 +21,21 @@ const Header = () => {
           </svg>
         </div>
 
-        <h1 className="text-center uppercase ">
+        <div className="w-full h-fit  sm:hidden block">
+          <svg viewBox="0 0 2100 550">
+            <text
+              className="text-[30rem]"
+              x="50%"
+              y="50%"
+              dy=".4em"
+              fill="transparent"
+              textAnchor="middle">
+              QUIDDLE
+            </text>
+          </svg>
+        </div>
+
+        <h1 className="text-center uppercase mt-5">
           Experience The Next Level of Words Game
         </h1>
         <Link href="/auth">
@@ -29,7 +43,7 @@ const Header = () => {
             whileHover={{ scale: 1.2 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: 'spring', stiffness: 300, damping: 15 }}
-            className="px-[10rem] py-4 mt-[4rem] rounded-md text-[1.5rem] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            className="px-[5rem] py-3 md:px-[10rem] md:py-4 mt-[2rem] md:mt-[4rem] rounded-md text-xl md:text-[1.5rem] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
             Play Now
           </motion.button>
         </Link>
